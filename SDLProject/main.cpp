7969eb474741495654f3ea1dde6b5ac0248c9bc9
yujiahou <houyujia9@gmail.com>
2025-02-28 00:46:29 -0500

    Updated main.cpp

--- SDLProject/main.cpp
+++ SDLProject/main.cpp
@@ -1,4 +1,12 @@
-
+/**
+* Author:Yujia Hou
+* Assignment: Pong Clone
+* Date due: 2025-3-01, 11:59pm
+* I pledge that I have completed this assignment without
+* collaborating with anyone else, in conformance with the
+* NYU School of Engineering Policies and Procedures on
+* Academic Misconduct.
+**/
 #define GL_SILENCE_DEPRECATION
 #define STB_IMAGE_IMPLEMENTATION
 #define LOG(argument) std::cout << argument << '\n'
